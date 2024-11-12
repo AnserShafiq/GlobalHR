536248b39f5bb0920e82c3ed0bdfeeb41fbfaddf
AnserShafiq <ansershafiq@gmail.com>
2024-11-12 19:52:31 +0500

    Website Completed

--- src/Pages/Applications/EmployersForm.jsx
+++ src/Pages/Applications/EmployersForm.jsx
@@ -4,9 +4,9 @@ import EmployersApplicationForm from '../../Components/Applications/EmployersApp
 const EmployersForm = () => {
   return (
     <div className='flex justify-start flex-col items-start mx-auto w-full lg:w-[1100px] px-4 lg:px-0 '>
-        <h2 className='text-[2.3rem]  font-semibold capitalize leading-tight mt-3 lg:mt-6'>Ready to take the next step in your career?</h2>
+        <h2 className='text-[2.3rem]  font-semibold capitalize leading-tight mt-3 lg:mt-6'>Ready to boost your business?</h2>
         <h4 className='text-[1.1rem] tracking-wide font-[300]'>
-          Use the form below to share your information, and together we can explore positions that align with your career path. The more details you provide, the closer we can get to finding your ideal match!!!
+        Use the form below to share your hiring needs, and let's collaborate to find skilled candidates who will elevate your business. The more details you provide, the closer we get to finding the ideal talent that aligns with your goals!!!
         </h4>
         <EmployersApplicationForm />
     </div>
